a7007ae5a3e1269b667a809fc3ff19e953814e4b
Sholastix <drenai6436@gmail.com>
2023-10-17 09:52:31 +0300

    'server.js': 'Multer' basic config added.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const multer  = require('multer');
+const path = require('path');
 
 const connectDB = require('./config/connectDB');
 
@@ -11,6 +13,56 @@ const routes = require('./routes/index');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Path to employee photos.
+const dir = './public/photos';
+
+// Path to default employee's photo.
+const defaultPhoto = './public/photos/default.jpg';
+
+// Here we configuring the storage and naming settings.
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Set the storage path.
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    // Name of each uploaded file must be unique in our fs/db, but we also want to preserve intact file's original name and extension. 
+    const parts = file.originalname.split('.');
+    cb(null, parts[0] + '_' + Date.now() + '.' + parts[1]);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  // File filter's config.
+  fileFilter: (req, file, cb) => {
+    // Create RegExp for allowed filetypes.
+    const allowedFiletypes = /jpg|jpeg|png/i;
+    // Checking extension of file which we want to upload.
+    const checkExtension = allowedFiletypes.test(path.extname(file.originalname));
+    // Checking mimetype of file which we want to upload (actually, this step is important, because file extension alone very easy to change).
+    const checkMimetype = allowedFiletypes.test(file.mimetype);
+    if (checkExtension && checkMimetype) {
+      return cb(null, true);
+    } else {
+      return cb('ERROR: This filetype not allowed. Use static images only!');
+    };
+  }
+});
+
+// @route: POST /api/upload
+// @desc: Files uploading.
+app.post('/upload', upload.single('image'), (req, res, next) => {
+  if (req.file !== undefined) {
+    console.log(`File uploaded successfully: "${req.file.destination}/${req.file.filename}".`, req.file);
+    next();
+  } else {
+    console.error('ERROR: There is nothing to upload!');
+    res.send('ERROR: There is nothing to upload!');
+    next();
+  };
+});
 
 // Set the routes.
 app.use('/api/', routes.authRoute);
