c8f85a8794454cd0ef086710a44e06bfc0ceec94
Sholastix <drenai6436@gmail.com>
2023-10-22 14:47:52 +0300

    server -> 'server.js': npm 'cors' added; cleaning.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const app = express();
-const multer  = require('multer');
-const path = require('path');
+// const multer  = require('multer');
+// const path = require('path');
 
 const connectDB = require('./config/connectDB');
 
@@ -12,58 +13,64 @@ const routes = require('./routes/api/index');
 // Initialize the middleware.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('./public'));
+// Сross-origin resource sharing permission middleware.
+app.use(cors());
 
-// Path to employee photos.
-const dir = './public/photos';
+////////////////// MULTER MIDDLEWARE. LATER IT WILL BE RELOCATED AND INTEGRATED INTO APP. //////////////////
 
-// Path to default employee's photo.
-const defaultPhoto = './public/photos/default.jpg';
+// // Path to employee photos.
+// const dir = './public/photos';
 
-// Here we configuring the storage and naming settings.
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // Set the storage path.
-    cb(null, dir);
-  },
-  filename: (req, file, cb) => {
-    // Name of each uploaded file must be unique in our fs/db, but we also want to preserve intact file's original name and extension. 
-    const parts = file.originalname.split('.');
-    cb(null, parts[0] + '_' + Date.now() + '.' + parts[1]);
-  }
-});
+// // Path to default employee's photo.
+// const defaultPhoto = './public/photos/default.jpg';
 
-const upload = multer({
-  storage: storage,
-  // File filter's config.
-  fileFilter: (req, file, cb) => {
-    // Create RegExp for allowed filetypes.
-    const allowedFiletypes = /jpg|jpeg|png/i;
-    // Checking extension of file which we want to upload.
-    const checkExtension = allowedFiletypes.test(path.extname(file.originalname));
-    // Checking mimetype of file which we want to upload (actually, this step is important, because file extension alone very easy to change).
-    const checkMimetype = allowedFiletypes.test(file.mimetype);
-    if (checkExtension && checkMimetype) {
-      return cb(null, true);
-    } else {
-      return cb('ERROR: This filetype not allowed. Use static images only!');
-    };
-  }
-});
+// // Here we configuring the storage and naming settings.
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // Set the storage path.
+//     cb(null, dir);
+//   },
+//   filename: (req, file, cb) => {
+//     // Name of each uploaded file must be unique in our fs/db, but we also want to preserve intact file's original name and extension. 
+//     const parts = file.originalname.split('.');
+//     cb(null, parts[0] + '_' + Date.now() + '.' + parts[1]);
+//   }
+// });
 
-// @route: POST /upload
-// @desc: Files uploading.
-app.post('/upload', upload.single('image'), (req, res, next) => {
-  if (req.file !== undefined) {
-    console.log(`File uploaded successfully: "${req.file.destination}/${req.file.filename}".`, req.file);
-    res.send(`File uploaded successfully: "${req.file.destination}/${req.file.filename}".`); // response for "POSTMAN".
-    next();
-  } else {
-    console.error('ERROR: There is nothing to upload!');
-    res.send('ERROR: There is nothing to upload!');
-    next();
-  };
-});
+// const upload = multer({
+//   storage: storage,
+//   // File filter's config.
+//   fileFilter: (req, file, cb) => {
+//     // Create RegExp for allowed filetypes.
+//     const allowedFiletypes = /jpg|jpeg|png/i;
+//     // Checking extension of file which we want to upload.
+//     const checkExtension = allowedFiletypes.test(path.extname(file.originalname));
+//     // Checking mimetype of file which we want to upload (actually, this step is important, because file extension alone very easy to change).
+//     const checkMimetype = allowedFiletypes.test(file.mimetype);
+//     if (checkExtension && checkMimetype) {
+//       return cb(null, true);
+//     } else {
+//       return cb('ERROR: This filetype not allowed. Use static images only!');
+//     };
+//   }
+// });
+
+// // @route: POST /upload
+// // @desc: Files uploading.
+// app.post('/upload', upload.single('image'), (req, res, next) => {
+//   if (req.file !== undefined) {
+//     console.log(`File uploaded successfully: "${req.file.destination}/${req.file.filename}".`, req.file);
+//     res.send(`File uploaded successfully: "${req.file.destination}/${req.file.filename}".`); // response for "POSTMAN".
+//     next();
+//   } else {
+//     console.error('ERROR: There is nothing to upload!');
+//     res.send('ERROR: There is nothing to upload!');
+//     next();
+//   };
+// });
+
+///////////////////////////////////////////////////////
 
 // Set the routes.
 app.use('/api/', routes.authRoute);
