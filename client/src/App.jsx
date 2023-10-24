513fabde1b224df5616120318b9dbad60ffdd978
Sholastix <drenai6436@gmail.com>
2023-10-24 19:09:47 +0300

    Redux store added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,20 +10,26 @@ import Navbar from './components/Navbar/Navbar';
 import Signin from './components/Signin/Signin';
 import Signup from './components/Signup/Signup';
 
+// Redux Setup
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path='/' element={<Navbar />}>
-          <Route index element={<Homepage />} />
-          <Route path='/employees-list' element={<EmployeesList />} />
-          <Route path='/signin' element={<Signin />} />
-          <Route path='/signup' element={<Signup />} />
-        </Route>
-        <Route path='/employees-create-form' element={<EmployeesCreateForm />} />
-        <Route path='/employees-edit-form' element={<EmployeesEditForm />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Navbar />}>
+            <Route index element={<Homepage />} />
+            <Route path='/employees-list' element={<EmployeesList />} />
+            <Route path='/signin' element={<Signin />} />
+            <Route path='/signup' element={<Signup />} />
+          </Route>
+          <Route path='/employees-create-form' element={<EmployeesCreateForm />} />
+          <Route path='/employees-edit-form' element={<EmployeesEditForm />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
