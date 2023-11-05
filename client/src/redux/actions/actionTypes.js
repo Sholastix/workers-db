48ec3a66b4bc7686e90d2cf052d2f06f22cfee84
Sholastix <drenai6436@gmail.com>
2023-11-05 15:13:04 +0200

    Cleaning.

--- client/src/redux/actions/actionTypes.js
+++ client/src/redux/actions/actionTypes.js
@@ -1,7 +1,5 @@
-// 'Alert' component.
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 
-// 'Signup' component.
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
