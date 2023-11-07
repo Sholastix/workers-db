e686009ce285097d53a92546b1fb51d681ae3e85
Sholastix <drenai6436@gmail.com>
2023-11-07 16:11:48 +0200

    Cleaning.

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -7,19 +7,21 @@ import setAuthToken from '../../utils/setAuthToken';
 // Check if user already signed in (basically we check if there is a token in Local Storage).
 export const isUserSigned = () => async dispatch => {
   try {
-    // If there is token in LS, then we put it in global header.
-    if (localStorage.token) {
-      setAuthToken();
-    };
+    // If there is token in LocalStorage, then we put it in global header.
+    // For this function we need our token in global headers cause in 'server -> routes -> auth.js' we get the user's ID exactly from this token which contains user's data.
+    // But we already pushing tokens from LS to global headers in 'App.jsx' component, so here we don't need to repeat it for now. So we leave it here inactive just for better understanding of all auth process. 
+    // if (localStorage.token) {
+    //   setAuthToken();
+    // };
 
     const user = await axios.get('http://localhost:5000/api/auth');
-    
+
     dispatch({
       type: USER_SIGNED_IN,
       payload: user.data
     });
 
-    // console.log('isUserSigned(): ', user.data);
+    console.log('isUserSigned(): ', user.data);
   } catch (err) {
     dispatch({
       type: AUTH_ERROR
@@ -42,6 +44,17 @@ export const signup = (props) => async dispatch => {
       type: SIGNUP_SUCCESS,
       payload: newUser.data
     });
+
+    // localStorage.setItem('token', newUser.data.signedToken);
+
+    // We set our general app in way that when new user finished his registration - he immediately received web-token.
+    // We need to insert this token into the global header right now so that the user can get the data from the protected routes without additional manipulations (page reload, signin etc.). 
+    if (localStorage.token) {
+      setAuthToken();
+      dispatch({
+        type: USER_SIGNED_IN
+      });
+    };
 
     // console.log({ 'NEW_USER_DATA: ': newUser.data }); // get user's data.
     // console.log({ 'TOKEN: ': newUser.data.signedToken }); // get the token.
