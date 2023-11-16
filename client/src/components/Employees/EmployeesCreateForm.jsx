ac2e51908675f82d63fc9675f641cc712ef7ee96
Sholastix <drenai6436@gmail.com>
2023-11-16 19:15:58 +0200

    CLIENT: create & edit form pages for employees connected to redux and auth protection for that pages added.

--- client/src/components/Employees/EmployeesCreateForm.jsx
+++ client/src/components/Employees/EmployeesCreateForm.jsx
@@ -1,6 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import cssStyles from './EmployeesCreateForm.module.css';
 
-const EmployeesCreateForm = () => {
+const EmployeesCreateForm = (props) => {
+
+  // Redirect to signin page if user signed out.
+  if (props.isAuthenticated === false) {
+    return <Navigate to='/signin' replace={true} />
+  };
+
   return (
     <header className={cssStyles.header}>
       <div>EMPLOYEES CREATE FORM</div>
@@ -8,4 +20,15 @@ const EmployeesCreateForm = () => {
   );
 };
 
-export default EmployeesCreateForm;
+EmployeesCreateForm.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+const mapDispatchToProps = null;
+
+// 'connect()' function connects a React component to a Redux store.
+export default connect(mapStateToProps, mapDispatchToProps)(EmployeesCreateForm);
--- client/src/components/Employees/EmployeesEditForm.jsx
+++ client/src/components/Employees/EmployeesEditForm.jsx
@@ -1,6 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import cssStyles from './EmployeesEditForm.module.css';
 
-const EmployeesEditForm = () => {
+const EmployeesEditForm = (props) => {
+
+  // Redirect to signin page if user signed out.
+  if (props.isAuthenticated === false) {
+    return <Navigate to='/signin' replace={true} />
+  };
+
   return (
     <header className={cssStyles.header}>
       <div>EMPLOYEES EDIT FORM</div>
@@ -8,4 +20,15 @@ const EmployeesEditForm = () => {
   );
 };
 
-export default EmployeesEditForm;
+EmployeesEditForm.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+const mapDispatchToProps = null;
+
+// 'connect()' function connects a React component to a Redux store.
+export default connect(mapStateToProps, mapDispatchToProps)(EmployeesEditForm);
