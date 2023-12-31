8107b3b43dba474e7506f2351c3120e8d53e6a7b
Sholastix <drenai6436@gmail.com>
2023-12-31 12:24:53 +0200

    CLIENT: component 'App.jsx' connected to Redux.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import cssStyles from './App.module.css';
 
@@ -15,51 +17,50 @@ import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import Signin from './components/Signin/Signin';
 import Signup from './components/Signup/Signup';
 
-// Redux Setup
-import { Provider } from 'react-redux';
-import store from './redux/store/store';
 import { isUserSigned } from './redux/actions/auth';
 
-// Check if user signed in when app starts first time.
-if (localStorage.token) {
-  // We use 'store.dispatch()' syntax here, because our App Component not connected to Redux store (we can connect it, but I want to leave it as it is for now).
-  store.dispatch(isUserSigned());
-};
-
-const App = () => {
+const App = (props) => {
   useEffect(() => {
-    // Check if user signed in every time when page reloaded.
-    store.dispatch(isUserSigned());
+    // Every time when page reloaded we check if user signed in.
+    props.isUserSigned();
   }, []);
 
   return (
-    <Provider store={store}>
-      <Router>
-        <div id={cssStyles.container}>
-          <div>
-            <Navbar />
-          </div>
-          <div className={cssStyles.section}>
-            <Alert />
-            <Routes>
-              <Route exact path='/' element={<Homepage />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path='/employees-create-form' element={<EmployeesCreateForm />} />
-                <Route path='/employees-edit-form/:id' element={<EmployeesEditForm />} />
-                <Route path='/employees-list' element={<EmployeesList />} />
-              </Route>
-              <Route path='/signin' element={<Signin />} />
-              <Route path='/signup' element={<Signup />} />
-              <Route path='*' element={<Page_404 />} />
-            </Routes>
-          </div>
-          <div className={cssStyles.footer}>
-            <Footer />
-          </div>
+    <Router>
+      <div id={cssStyles.container}>
+        <div>
+          <Navbar />
         </div>
-      </Router>
-    </Provider>
+        <div className={cssStyles.section}>
+          <Alert />
+          <Routes>
+            <Route exact path='/' element={<Homepage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path='/employees-create-form' element={<EmployeesCreateForm />} />
+              <Route path='/employees-edit-form/:id' element={<EmployeesEditForm />} />
+              <Route path='/employees-list' element={<EmployeesList />} />
+            </Route>
+            <Route path='/signin' element={<Signin />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='*' element={<Page_404 />} />
+          </Routes>
+        </div>
+        <div className={cssStyles.footer}>
+          <Footer />
+        </div>
+      </div>
+    </Router>
   );
 };
 
-export default App;
+App.propTypes = {
+  isUserSigned: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = {
+  isUserSigned
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './App.jsx';
+
+import { isUserSigned } from './redux/actions/auth.js';
+import store from './redux/store/store';
+
+// Check if user signed in when app starts first time.
+if (localStorage.token) {
+  store.dispatch(isUserSigned());
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  // </React.StrictMode>,
+  </Provider>
 );
