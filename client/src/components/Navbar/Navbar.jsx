6e5ac282730ce728d157ab488ec2cdcc7ad33772
Sholastix <drenai6436@gmail.com>
2024-02-29 15:26:47 +0200

    CLIENT: npm 'font awesome' installed; username & dropdown menu added to navbar.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
-import { Fragment } from 'react'
+import { useState, useEffect, useRef, Fragment } from 'react'
 import { Link, Outlet } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from 'prop-types';
 
 import cssStyles from './Navbar.module.css';
@@ -8,27 +10,46 @@ import cssStyles from './Navbar.module.css';
 import { signout } from '../../redux/actions/auth';
 
 const Navbar = (props) => {
+  // State od dropsown menu.
+  const [active, setActive] = useState(false);
+
+  const menuRef = useRef();
+
+  useEffect(() => {
+    // Close dropdown menu by mouse button clicking on everywhere on the window.
+    const dropdownHandler = (event) => {
+      try {
+        if (menuRef.current !== undefined && menuRef.current !== null && !menuRef.current.contains(event.target)) {
+          setActive(false);
+        };
+      } catch (err) {
+        console.error(err);
+      };
+    };
+
+    document.addEventListener('mouseup', dropdownHandler);
+  }, []);
+
   // Here we gather together links which authenticated and authorized users will see.
   const userLinks = (
     <Fragment>
-      <Link className={cssStyles.navRefs} to='/employees-list'>
-        <li>Employees</li>
+      <Link className={cssStyles.navRefs} to='/employees-list'>Employees</Link>
+      <Link ref={menuRef} className={cssStyles.navRefs} onClick={() => { setActive(!active) }} to='#'>
+        {props.user !== null && props.user.username} <FontAwesomeIcon icon={active === true ? faCaretUp : faCaretDown} size='xs' />
       </Link>
-      <Link className={cssStyles.navRefs} to='#'>
-        <li onClick={props.signout}>{props.user !== null && props.user.username} (signout)</li>
-      </Link>
+      <div className={active === true ? cssStyles.dropdownActive : cssStyles.dropdownInactive} >
+        <ul>
+          <li onClick={props.signout}><Link className={cssStyles.dropdownItem} to='/#'>SignOut</Link></li>
+        </ul>
+      </div>
     </Fragment>
   );
 
   // Here we gather together links which guests will see.
   const guestLinks = (
     <Fragment>
-      <Link className={cssStyles.navRefs} to='/signin'>
-        <li>SignIn</li>
-      </Link>
-      <Link className={cssStyles.navRefs} to='/signup'>
-        <li>SignUp</li>
-      </Link>
+      <Link className={cssStyles.navRefs} to='/signin'>SignIn</Link>
+      <Link className={cssStyles.navRefs} to='/signup'>SignUp</Link>
     </Fragment>
   );
 
